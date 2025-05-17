b28e761d6be79f73e30ca68984cedd2126b11f4e
Abhishek Kumar <abhishekcusat21@gmail.com>
2025-05-17 12:32:54 +0530

    update config

--- discovery-service/src/test/java/com/discovery/DiscoveryServiceApplicationTests.java
+++ discovery-service/src/test/java/com/discovery/DiscoveryServiceApplicationTests.java
@@ -1,13 +1,11 @@
-//package com.discovery;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class DiscoveryServiceApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+package com.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class DiscoveryServiceApplicationTests {
+
+@Test
+void contextLoads() {
+ }
+}
